28f6845f6e0066b18ecbbf40e25230cf5a0d1aaa
blloyd08 <blloyd_2004@hotmail.com>
2019-10-17 18:59:24 -0700

    Change discord api wrapper  to Discord.js

--- commands/strat.js
+++ commands/strat.js
@@ -1,19 +1,16 @@
 var request = require('request');
 
-module.exports.strat =  (bot, messageArgs) => {
+module.exports.strat =  (message) => {
     request.post('https://squadstrats.com/wp-content/themes/squat/twooff_data.php',callback = (error, res, body) => {
         var strat = JSON.parse(body);
-        displayStrat(bot, messageArgs.channelID, strat);
+        displayStrat(message.channel, strat);
     });
 }
 
-function displayStrat(bot, channelID, strat){
+function displayStrat(channel, strat){
     var attack = strat[0];
     var attachMessage = `Attack\n${attack.title}\n${attack.strat}\n`
     var defend = strat[1];
     var defendMessage = `Defend\n${defend.title}\n${defend.strat}`
-    bot.sendMessage({
-        to: channelID,
-        message: `${attachMessage}\n${defendMessage}`
-    })
+    channel.send(`${attachMessage}\n${defendMessage}`);
 }
